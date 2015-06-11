e5a9b75767584a0724460432a43ea85623476d88
Imuli <i@imu.li>
2015-06-11 17:23:06 -0400

    add bits

--- client/bit.js
+++ client/bit.js
@@ -1,5 +1,29 @@
 Meteor.subscribe("bits");
 
+Template.newbit.helpers({
+	newtype: function() {
+		return "new"+this.type;
+	},
+	displaytype: function() {
+		return this.type.charAt(0).toUpperCase() + this.type.slice(1);
+	},
+});
+
+Template.newbit.events({
+	"submit": function(event) {
+		var obj = {};
+		var ele = event.target.elements;
+		for(var i=0; i<ele.length; i++){
+			obj[ele[i].name] = ele[i].value;
+			ele[i].value = "";
+		}
+		delete(obj[""]);
+		obj.tags = []; /* FIXME, all currently selected tags */
+		Meteor.call("addBit", obj);
+		return false;
+	},
+});
+
 Template.bits.helpers({
 	bits: function() {
 		return Bits.find({type: this.type});
