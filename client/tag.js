e4bc035e4e2756852bce3993a2666d19075137b8
Imuli <i@imu.li>
2015-06-11 20:06:15 -0400

    add tags with colors!

--- client/tag.js
+++ client/tag.js
@@ -1,14 +1,27 @@
 Meteor.subscribe("tags");
 
-Template.body.events({
-	"submit .new-tag": function(event) {
+Template.newtag.events({
+	"submit": function(event) {
 		Meteor.call("addTag",
 				event.target.title.value,
 				event.target.color.value);
 		event.target.title.value="";
 		event.target.color.value="";
 		return false;
-	}
+	},
+	"click .color": function(event) {
+		var target = event.target;
+		var colors = hilbertColor(function(color){
+			target.style.background = color;
+			target.value = color;
+			colors.parentNode.removeChild(colors);
+		});
+		colors.style.position = "absolute";
+		document.body.appendChild(colors);
+		colors.style.top = (target.offsetTop + target.offsetHeight ) + "px";
+		colors.style.left = (target.offsetLeft + target.offsetWidth/2 - colors.offsetWidth/2) + "px";
+		return false;
+	},
 });
 
 Template.tags.helpers({
@@ -49,7 +62,6 @@ Template.tag.events({
 	"dragend": function(event) {
 		var dt = event.originalEvent.dataTransfer;
 		var node = event.target.parentNode.parentNode;
-		console.log(node);
 		if(dt.dropEffect == "move" && node.classList[0] === "bit"){
 			Meteor.call("delTagFromBit", dt.getData('text/tag'), node.id); 
 		}
