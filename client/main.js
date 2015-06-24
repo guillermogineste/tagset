e5b879f5e9bb95f62d78229b59de5ba839987dbd
Guillermo Gineste <guillermogineste@users.noreply.github.com>
2015-06-24 15:21:41 +0200

    Layout
    Changes in layout:
    — New top bar in each column, future UI elements could also be in this
    bar
    — Now the forms collapse and expand by clicking add button so it
    doesn’t take that much space from the content
    
    Other fixes:
    — Links go to new tab

--- client/main.js
+++ client/main.js
@@ -11,6 +11,16 @@ Template.body.events({
 	"click .btn--tags-filter": function() {
 		$(".tags-filter").toggle();
 	},
+	"click .add-expand": function(e) {
+		var $this = $(e.target);
+		$this.parent().siblings('.col__add-form__form').toggle();
+	    $this.toggleClass('close');
+		if($this.hasClass('close')){
+			$this.text('Close');
+		} else {
+			$this.text('Add');
+		}
+	},
 });
 
 Accounts.ui.config({
